72d416a618bacc2fc1b0aa60969f88a34e2a8d5d
prakash214 <prakashthapa1998@gmail.com>
2022-09-08 14:16:01 +0000

    test sh changed

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 declare -a directories=("front-end" "fruit" "friend" "characters")
 sudo apt-get update
-sudo apt-get install python3 python3-pip python3-venv
+sudo apt-get -y install python3 python3-pip python3-venv
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r test_requirements.txt
