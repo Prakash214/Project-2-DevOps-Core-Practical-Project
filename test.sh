67399bb0f8adfdac6858b1b8bbe93ad06203aea5
prakash214 <prakashthapa1998@gmail.com>
2022-09-08 10:21:11 +0000

    minor changes in test script

--- test.sh
+++ test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 declare -a directories=("front-end" "fruit" "friend" "characters")
+sudo apt-get update
+sudo apt-get install python3 python3-pip python3-venv
+source venv/bin/activate
+pip3 install -r test_requirements.txt
+python3 -m venv venv
 for dir in "${directories[@]}"
 do
   cd ${dir}
-  sudo apt-get update
-  sudo apt-get install python3 python3-pip python3-venv
-  python3 -m venv venv
-  source venv/bin/activate
-  pip3 install -r test_requirements.txt
+  pwd
   python3 -m pytest --cov=application 
-  deactivate
+  pwd
   cd ..
 done
